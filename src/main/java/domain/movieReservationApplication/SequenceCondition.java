1a94a8e09903b386575e9d7dfce2fdfb80087d96
k9want <zndwlrnjr26@daum.net>
2023-11-27 02:34:12 +0900

    refactor: DiscountCondition 다형성을 통해 개선
    -할인 조건:DiscountCondition<<interface>>
    -순번 조건:PeriodCondition
    -기간 조건:SequenceCondition

---
+++ src/main/java/domain/movieReservationApplication/SequenceCondition.java
@@ -0,0 +1,14 @@
+package domain.movieReservationApplication;
+
+public class SequenceCondition implements DiscountCondition{
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
