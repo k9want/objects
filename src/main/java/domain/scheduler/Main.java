bf0947834e48ad33ab9e4b6af3b7761ef850a3fb
k9want <zndwlrnjr26@daum.net>
2023-11-29 19:43:20 +0900

    refactor: isSatisfied 부수효과 제거
    -reschedule: private -> public으로 변경
    -isSatisfied 오직 쿼리로써 부수효과 없이 반환값만 반환하도록 변경
    -명령-쿼리 분리 원칙 적용

--- src/main/java/domain/scheduler/Main.java
+++ src/main/java/domain/scheduler/Main.java
@@ -7,9 +7,15 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        Event meeting = new Event("회의", LocalDateTime.of(2019, 5, 8, 10, 30),
+        Event event = new Event("회의", LocalDateTime.of(2019, 5, 9, 10, 30),
                 Duration.ofMinutes(30));
         RecurringSchedule schedule = new RecurringSchedule("회의", DayOfWeek.WEDNESDAY, LocalTime.of(10, 30),
                 Duration.ofMinutes(30));
+
+        System.out.println("reschedule 전: " + event.isSatisfied(schedule));
+        if (!event.isSatisfied(schedule)) {
+            event.reschedule(schedule);
+            System.out.println("reschedule 후: " + event.isSatisfied(schedule));
+        }
     }
 }
