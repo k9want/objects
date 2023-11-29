81e0933e1b31e04271d4c2fb8e46884270280e52
k9want <zndwlrnjr26@daum.net>
2023-11-29 17:25:48 +0900

    feat: 영화 예매 애플리케이션 실행

--- src/main/java/domain/movieReservationApplication/Reservation.java
+++ src/main/java/domain/movieReservationApplication/Reservation.java
@@ -12,4 +12,8 @@ public class Reservation {
         this.fee = fee;
         this.audienceCount = audienceCount;
     }
+
+    public Money getFee() {
+        return fee;
+    }
 }
---
+++ src/main/java/domain/movieReservationApplication/ReservationAgency.java
@@ -0,0 +1,20 @@
+package domain.movieReservationApplication;
+
+import domain.movieReservationApplication.discountCondition.SequenceCondition;
+import domain.movieReservationApplication.discountPolicy.AmountDiscountPolicy;
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ReservationAgency {
+    public static void main(String[] args) {
+        DiscountPolicy discountPolicy = new AmountDiscountPolicy(Money.wons(2_000),
+                new SequenceCondition(1));
+        Movie movie = new Movie("The Great Movie", Duration.ofMinutes(120), new Money(new BigDecimal(15000)), discountPolicy);
+        Screening screening = new Screening(movie, 1, LocalDateTime.of(2023, 11, 27, 14, 30));
+        Customer customer = new Customer("A", "1");
+
+        Reservation reserve = screening.reserve(customer, 1);
+        System.out.println(reserve.getFee());
+    }
+}
